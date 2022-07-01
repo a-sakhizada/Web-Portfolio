76d643ce704530634f19b5257da4cddfbb2bc855
a-sakhizada <arezousakhizada@gmail.com>
2022-06-30 22:00:03 -0400

    added testimonial reviews from clients/references

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,7 +98,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*======================= PORTFOLIO SWIPER ========================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
@@ -110,8 +110,25 @@ let swiper = new Swiper('.portfolio__container', {
       el: '.swiper-pagination',
       clickable: true,
     },
-  });
+});
 
+/*======================= TESTIMONIAL ========================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+});
 
 
 
